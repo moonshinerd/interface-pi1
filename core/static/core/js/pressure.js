31d9ff1f14cffce6a757e1acc3a9860205320f32
Letícia Resende <letigy78@gmail.com>
2025-05-14 22:53:31 -0300

    adicionando css
    Co-authored-by: Carolina Barbosa <carolinabarbosab09@gmail.com>

--- core/static/core/js/pressure.js
+++ core/static/core/js/pressure.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // Simula o cálculo
     setTimeout(() => {
       const dist = select.value;
-      resultDiv.innerHTML = `<strong>Distância selecionada:</strong> ${dist} m`;
+      resultDiv.innerHTML = `<span style="color:rgb(134, 15, 106);"><strong>Distância selecionada:</strong> ${dist} m</span>`;
       resultDiv.classList.add('active');
 
       // Restaura o botão

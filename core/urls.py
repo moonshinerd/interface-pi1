0f9d9970209042252291694b2d700303be6e890d
Letícia <89492943+LeticiaResende23@users.noreply.github.com>
2025-05-21 15:57:20 -0300

    Iniciando endpoint do grafico teste
    Co-authored-by: Hauedy <hauedyws@gmail.com>

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 # urls.py (correto)
 from django.urls import path
 from django.views.generic import TemplateView
+from . import views
 
 
 
@@ -9,4 +10,5 @@ urlpatterns = [
     path('oldlaunches/', TemplateView.as_view(template_name="core/launch_list.html")),
     path('home/', TemplateView.as_view(template_name="core/home.html")),
     path('', TemplateView.as_view(template_name="core/base.html")),
+    path('graficosteste/', views.graficos_teste, name='graficosteste'),
 ]   

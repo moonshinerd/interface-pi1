10ee3b4d84f5b716235c3563fee0715734557906
HauedyWS <hauedyws@gmail.com>
2025-05-21 16:49:35 -0300

    continuacao do endpoint do grafico de teste
    Co-authered: LeticiaResende23 <letigy78@gmail.com>

--- core/utils.py
+++ core/utils.py
@@ -1,1 +1,17 @@
 
+import plotly.graph_objects as go
+
+def criar_figura_aceleracao(times, accel_x):
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(
+        x=times,
+        y=accel_x,
+        mode='lines+markers',
+        name='Aceleração X',
+        line=dict(color='#3a5f6f') # define a cor da linha
+    ))
+    fig.update_layout(
+        xaxis_title='Data/Hora',
+        yaxis_title='Aceleração (m/s²)'
+    )
+    return fig

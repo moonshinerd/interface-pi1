085ecd4346edbb98d20273fb0935c4246fa067a2
moonshinerd <vitorhls15@gmail.com>
2025-05-26 11:56:10 -0300

    feat: implement 2D GPS map visualization in launch details

--- core/utils.py
+++ core/utils.py
@@ -15,3 +15,32 @@ def criar_figura_aceleracao(times, accel_x):
         yaxis_title='Aceleração (m/s²)'
     )
     return fig
+
+
+def criar_figura_gps_mapa(telemetrias) -> go.Figure:
+    telemetrias = list(telemetrias)
+    if not telemetrias:
+        return go.Figure()
+
+    lats = [t.latitude for t in telemetrias]
+    lons = [t.longitude for t in telemetrias]
+
+    fig = go.Figure(go.Scattermapbox(
+        lat=lats,
+        lon=lons,
+        mode='lines+markers',    # desenha rota e pontos
+        marker=dict(size=6),
+        line=dict(width=2)
+    ))
+
+    fig.update_layout(
+        mapbox_style='open-street-map',
+        mapbox=dict(
+            center=dict(lat=sum(lats)/len(lats), lon=sum(lons)/len(lons)),
+            zoom=18.499999999999998, 
+        ),
+        uirevision='gps-map',
+        margin=dict(l=0, r=0, t=30, b=0),
+        title='Trajetória GPS'
+    )
+    return fig
--- core/views.py
+++ core/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from plotly.io import to_html
 from .models import Lancamento
-from .utils import criar_figura_aceleracao
+from .utils import criar_figura_aceleracao, criar_figura_gps_mapa
 
 def graficos_teste(request):
     lanc = Lancamento.objects.prefetch_related('telemetrias').first()
@@ -17,10 +17,15 @@ def graficos_teste(request):
 
 def detalhe_lancamento(request, pk):
     lancamento = get_object_or_404(Lancamento, pk=pk)
-    telemetrias = lancamento.telemetrias.all()
+    telemetrias = lancamento.telemetrias.order_by('data_hora')
+    
+    # cria mapa GPS 2D
+    gps_fig      = criar_figura_gps_mapa(telemetrias)
+    gps_map_html = gps_fig.to_html(full_html=False, include_plotlyjs='cdn')
     return render(request, 'oldlaunches/detail.html', {
         'lancamento': lancamento,
-        'telemetrias': telemetrias
+        'telemetrias': telemetrias,
+        'gps_map_plot_html': gps_map_html,
     })
 
 def lista_lancamentos(request):

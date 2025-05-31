ff29f49f3a1452ed1deb19638682bc84cf55b4a8
xzxjesse <jessicaeveline121@gmail.com>
2025-05-31 09:10:51 -0300

    Adiciona a classe LancamentoModelTest em tests.py

--- core/tests.py
+++ core/tests.py
@@ -1,6 +1,14 @@
 from django.test import TestCase
-from core.models import Lancamento
+from core.models import Lancamento, Telemetria
 from django.utils import timezone
+from core.utils import (
+    criar_figura_aceleracao_x_tempo,
+    criar_figura_gps_3d,
+    criar_figura_tensao_potencia,
+    criar_figura_gps_mapa,
+    criar_figura_velocidade_angular
+)
+import plotly.graph_objects as go
 
 class OldLaunchesViewTest(TestCase):
     def setUp(self):
@@ -10,7 +18,7 @@ class OldLaunchesViewTest(TestCase):
             data_hora_fim=now,
             volume_agua=100,
             angulo=45,
-            pressao_ajustada=30,
+            pressao_lancamento=30,
             distancia_alvo=10,
         )
         self.lancamento2 = Lancamento.objects.create(
@@ -18,7 +26,7 @@ class OldLaunchesViewTest(TestCase):
             data_hora_fim=now,
             volume_agua=150,
             angulo=60,
-            pressao_ajustada=40,
+            pressao_lancamento=40,
             distancia_alvo=20,
         )
 
@@ -28,3 +36,26 @@ class OldLaunchesViewTest(TestCase):
         content = response.content.decode('utf-8')
         self.assertIn(str(self.lancamento1), content)
         self.assertIn(str(self.lancamento2), content)
+
+class LancamentoModelTest(TestCase):
+    def test_criar_lancamento_valido(self):
+        lancamento = Lancamento.objects.create(
+            data_hora_inicio=timezone.now(),
+            data_hora_fim=timezone.now(),
+            volume_agua=750.0,
+            angulo=45.0,
+            pressao_lancamento=53.03,
+            distancia_alvo=10.0
+        )
+        self.assertIsNotNone(lancamento.id_lancamento)
+        self.assertEqual(lancamento.volume_agua, 750.0)
+        self.assertEqual(lancamento.angulo, 45.0)
+
+    def test_distancia_alvo_choices(self):
+        lancamento = Lancamento.objects.create(
+            data_hora_inicio=timezone.now(),
+            data_hora_fim=timezone.now(),
+            distancia_alvo=20.0
+        )
+        self.assertIn(lancamento.distancia_alvo, [10.0, 20.0, 30.0])
+

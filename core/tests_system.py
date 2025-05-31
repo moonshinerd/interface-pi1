74d628be645bc74a8bc2ee2d7f7b7e66f482182d
xzxjesse <jessicaeveline121@gmail.com>
2025-05-31 09:23:35 -0300

    Adiciona o método setUp na classe SystemTest em tests_system.py

---
+++ core/tests_system.py
@@ -0,0 +1,23 @@
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.utils import timezone
+from core.models import Lancamento, Telemetria
+from core.utils import (
+    criar_figura_aceleracao_x_tempo,
+    criar_figura_gps_3d,
+    criar_figura_tensao_potencia,
+    criar_figura_gps_mapa,
+    criar_figura_velocidade_angular
+)
+
+class SystemTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.lancamento = Lancamento.objects.create(
+            data_hora_inicio=timezone.now(),
+            data_hora_fim=timezone.now(),
+            volume_agua=750.0,
+            angulo=45.0,
+            pressao_lancamento=53.03,
+            distancia_alvo=10.0
+        )

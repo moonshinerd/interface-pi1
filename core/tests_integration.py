6598633295b06d06963f1bd2cb70672772bc727c
xzxjesse <jessicaeveline121@gmail.com>
2025-05-31 09:21:18 -0300

    Adiciona a classe DataVisualizationIntegrationTest em tests_integration.py

--- core/tests_integration.py
+++ core/tests_integration.py
@@ -110,3 +110,59 @@ class ViewsIntegrationTest(TestCase):
         response = self.client.get(f'/oldlaunches/{self.lancamento.id_lancamento}/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'oldlaunches/detail.html')
+
+class DataVisualizationIntegrationTest(TestCase):
+    def setUp(self):
+        self.lancamento = Lancamento.objects.create(
+            data_hora_inicio=timezone.now(),
+            data_hora_fim=timezone.now(),
+            volume_agua=750.0,
+            angulo=45.0,
+            pressao_lancamento=53.03,
+            distancia_alvo=10.0
+        )
+        
+        self.telemetrias = []
+        for i in range(10):
+            telemetria = Telemetria.objects.create(
+                lancamento=self.lancamento,
+                data_hora=timezone.now(),
+                aceleracao_x=float(i),
+                aceleracao_y=float(i + 1),
+                aceleracao_z=float(i + 2),
+                vel_angular_x=float(i),
+                vel_angular_y=float(i + 1),
+                vel_angular_z=float(i + 2),
+                latitude=-23.5505 + (i * 0.0001),
+                longitude=-46.6333 + (i * 0.0001),
+                altitude=100.0 + i,
+                shunt_voltage=3.3 + i,
+                bus_voltage=5.0 + i,
+                current_mA=100.0 + i,
+                power_mW=500.0 + i
+            )
+            self.telemetrias.append(telemetria)
+
+    def test_visualizacoes_com_dados_reais(self):
+        """Testa todas as visualizações com dados reais e variados"""
+        telemetrias = self.lancamento.telemetrias.all()
+
+        fig_acel = criar_figura_aceleracao_x_tempo(telemetrias)
+        self.assertEqual(len(fig_acel.data), 3)
+        self.assertEqual(len(fig_acel.data[0].x), 10)
+
+        fig_gps = criar_figura_gps_3d(telemetrias)
+        self.assertEqual(len(fig_gps.data), 1)
+        self.assertEqual(len(fig_gps.data[0].x), 10)
+
+        fig_tensao = criar_figura_tensao_potencia(telemetrias)
+        self.assertEqual(len(fig_tensao.data), 4)
+        self.assertEqual(len(fig_tensao.data[0].x), 10)
+
+        fig_mapa = criar_figura_gps_mapa(telemetrias)
+        self.assertEqual(len(fig_mapa.data), 1)
+        self.assertEqual(len(fig_mapa.data[0].lat), 10)
+
+        fig_vel = criar_figura_velocidade_angular(telemetrias)
+        self.assertEqual(len(fig_vel.data), 3)
+        self.assertEqual(len(fig_vel.data[0].x), 10) 
